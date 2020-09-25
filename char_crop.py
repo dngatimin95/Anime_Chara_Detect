be416669a221df2aa182a4dad1c2e558551acbc3
dngatimin <dngatimin95@gmail.com>
2020-09-24 23:00:42 -0400

    Code update

--- char_crop.py
+++ char_crop.py
@@ -10,7 +10,7 @@ def crop(file_path, cascade = "lbpcascade_animeface.xml"):
     if not os.path.isfile(cascade):
         raise RuntimeError("%s: not found" % cascade)
 
-    faceCascade = cv2.CascadeClassifier(cascade)
+    face_cascade = cv2.CascadeClassifier(cascade)
 
     for file_name in os.listdir(file_path):
         img = cv2.imread(os.path.join(file_path,file_name), cv2.IMREAD_COLOR)
@@ -20,7 +20,7 @@ def crop(file_path, cascade = "lbpcascade_animeface.xml"):
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             gray = cv2.equalizeHist(gray)
 
-            find_faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 5, minSize = (30, 30))
+            find_faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 5, minSize = (30, 30))
             for (x, y, w, h) in find_faces:
                 crop_img = img[y:y+h, x:x+w]
                 resized_img = cv2.resize(crop_img, (width, height), interpolation=cv2.INTER_AREA)
--- char_detect.py
+++ char_detect.py
@@ -7,68 +7,56 @@ from PIL import Image
 import urllib.request
 import time
 
-
-url = 'http://10.0.1.107:8080/shot.jpg' # Insert Ip for webcam
-
-path = input("Enter the file path to the character dataset: ")
+url = input("Enter the webcam's IPv4 address (e.g. 123.123.12.1:3000): ") # Insert Ip for webcam
+url = "http://" + url + "/shot.jpg"
+file_path = input("Enter the file path to the character dataset: ")
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-faceCascade = cv2.CascadeClassifier('lbpcascade_animeface.xml')
+face_cascade = cv2.CascadeClassifier('lbpcascade_animeface.xml')
 
-def getImagesAndLabels(path):
-    charFolderPath = []
-    idCount = 1
-    charId = {}
-    faceSamples=[]
-    ids = []
-    for charFiles in os.listdir(path):
-        charId[charFiles] = idCount
-        charFolderPath.append(os.path.join(path,charFiles))
-        idCount += 1
+def get_char(file_path):
+    char_folder_path, char_face = [], []
+    ids = set()
+    char_id = 1
+    char_dic = {}
 
-    for charImages in charFolderPath:
-        imagePaths = [os.path.join(charImages,img) for img in os.listdir(charImages)]
-        for imagePath in imagePaths:
-            #store images directly
-            charName = os.path.basename(os.path.dirname(imagePath))
-            id = charId[charName]
-            image = cv2.imread(imagePath, cv2.IMREAD_COLOR)
+    for char_folder in os.listdir(file_path):
+        char_dic[char_folder] = char_id
+        char_folder_path.append(os.path.join(file_path,char_folder))
+        char_id += 1
+
+    for char_imgs in char_folder_path:
+        all_img_paths = [os.path.join(char_imgs, img) for img in os.listdir(char_imgs)]
+        for img_path in all_img_paths:
+            char_name = os.path.basename(os.path.dirname(img_path))
+            id = char_dic[char_name]
+            image = cv2.imread(img_path, cv2.IMREAD_COLOR)
             if image is not None:
                 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                 gray = cv2.equalizeHist(gray)
-                faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 5, minSize = (30, 30))
+                faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 5, minSize = (30, 30))
                 for (x, y, w, h) in faces:
                     crop_img = gray[y:y+h, x:x+w]
                     resized_image = cv2.resize(crop_img, (100, 100), interpolation=cv2.INTER_AREA)
-                    faceSamples.append(resized_image)
+                    char_face.append(resized_image)
                     print(id)
                     ids.append(id)
                     # SHOULD BE BETTER TO SCRAPE AND CROP TO LIMIT SIZE OF FILES
+    return char_face, ids, char_dic
 
-            #PIL_img = Image.open(imagePath).convert('L') # grayscale
-            #img_numpy = np.array(PIL_img,'uint8')
-            #charName = os.path.basename(os.path.dirname(imagePath))
-            #id = charId[charName]
-            #faces = faceCascade.detectMultiScale(img_numpy)
-            #for (x,y,w,h) in faces:
-            #    faceSamples.append(img_numpy[y:y+h,x:x+w])
-            #    ids.append(id)
-    print(ids)
-    return faceSamples, ids, charId
 
-print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
-faces, ids, charId = getImagesAndLabels(path)
+print ("\n [INFO] Training faces. This will take a few seconds. Wait...")
+faces, ids, char_dic = get_char(file_path)
 recognizer.train(faces, np.array(ids))
 recognizer.save('trainer.yml')
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
 
 recognizer.read('trainer.yml')
-font = cv2.FONT_HERSHEY_SIMPLEX
-#capture = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_DUPLEX
+#capture = cv2.VideoCapture(0) #Uncomment if using computer camera
 
-id = 0
-names = list(charId.keys())
+#id = 0
+names = list(char_dic.keys())
 names.insert(0, "None")
-print(names)
 
 # Define min window size to be recognized as a face
 #minW = 0.1*capture.get(3)
@@ -100,10 +88,10 @@ while True:
         cv2.putText(frame, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)
 
     cv2.imshow('AnimeFaceDetect',frame)
-    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
-    if k == 27:
+    esc = cv2.waitKey(10) & 0xff
+    if esc == 27:
         break
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup stuff")
-capture.release()
+
+print("\n [INFO] Exiting Program Now...")
+#capture.release() #Uncomment if using computer camera
 cv2.destroyAllWindows()
